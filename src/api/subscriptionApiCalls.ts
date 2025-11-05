9a33b843a4e11556abbc5054854a3cf55884573b
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-11-05 15:08:01 +0100

    feat(core): add api calls

---
+++ src/api/subscriptionApiCalls.ts
@@ -0,0 +1,50 @@
+import {subscriptionApi} from './axios.ts';
+import type {SubscriptionFilterDto} from '../modules/filter/SubscriptionFilterDto.ts';
+import type {CreateSubscriptionDto} from '../modules/types/subscription/dto/CreateSubscriptionDto.ts';
+import type {UpdateSubscriptionDto} from '../modules/types/subscription/dto/UpdateSubscriptionDto.ts';
+import type {
+  SubscriptionViewModel
+} from "../modules/types/subscription/view-model/subscription/SubscriptionViewModel.ts";
+
+export const subscriptionApiCalls = {
+  getById: async (id: string): Promise<SubscriptionViewModel> => {
+    const { data } = await subscriptionApi.get<SubscriptionViewModel>(`/${id}`);
+    return data;
+  },
+
+  getAll: async (filter?: SubscriptionFilterDto): Promise<SubscriptionViewModel[]> => {
+    const { data } = await subscriptionApi.get<SubscriptionViewModel[]>('', {
+      params: filter,
+    });
+    return data;
+  },
+
+  create: async (userId: string, dto: CreateSubscriptionDto): Promise<SubscriptionViewModel> => {
+    const { data } = await subscriptionApi.post<SubscriptionViewModel>(`/${userId}`, dto);
+    return data;
+  },
+
+  update: async (id: string, dto: UpdateSubscriptionDto): Promise<SubscriptionViewModel> => {
+    const { data } = await subscriptionApi.put<SubscriptionViewModel>(`/${id}`, dto);
+    return data;
+  },
+
+  cancel: async (subscriptionId: string, userId: string): Promise<SubscriptionViewModel> => {
+    const { data } = await subscriptionApi.patch<SubscriptionViewModel>(`/${subscriptionId}/cancel`, null, {
+      params: { userId },
+    });
+    return data;
+  },
+
+  renew: async (subscriptionId: string, monthsToRenew: number): Promise<SubscriptionViewModel> => {
+    const { data } = await subscriptionApi.patch<SubscriptionViewModel>(`/${subscriptionId}/renew`, null, {
+      params: { monthsToRenew },
+    });
+    return data;
+  },
+
+  getUpcomingBills: async (userId: string): Promise<SubscriptionViewModel[]> => {
+    const { data } = await subscriptionApi.get<SubscriptionViewModel[]>(`/bills/users/${userId}`);
+    return data;
+  },
+};
