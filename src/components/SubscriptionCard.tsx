686ffb474cece6b24aa016757237e3eaaf2e0ffd
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2026-01-22 14:13:36 +0100

    add SubscriptionCard component

--- src/components/SubscriptionCard.tsx
+++ src/components/SubscriptionCard.tsx
@@ -1,5 +1,6 @@
 import {SubscriptionType} from "../modules/types/enums/SubscriptionType.ts";
 import {SubscriptionContent} from "../modules/types/enums/SubscriptionContent.ts";
+import {Card, CardContent, Divider, Stack, Typography} from "@mui/material";
 
 type StartPageProps = {
   subscriptionName: string;
@@ -10,11 +11,22 @@ type StartPageProps = {
 export function SubscriptionCard ({subscriptionName, subscriptionType, subscriptionContent}: StartPageProps) {
 
   return (
-    <div className="subscription-card">
-      <h3>{subscriptionName}</h3>
-      <p>Type: {subscriptionType}</p>
-      <p>Content: {subscriptionContent}</p>
-    </div>
+      <Card variant="outlined">
+        <Stack direction="row" sx={{justifyContent: 'space-between', alignItems: 'center', padding: '10px'}}>
+          <Typography gutterBottom variant="h4" component="div">
+            {subscriptionName}
+          </Typography>
+          <Typography gutterBottom component="div" sx={{color: 'text.secondary'}}>
+            {subscriptionType}
+          </Typography>
+        </Stack>
+        <Divider />
+        <CardContent>
+          <Stack direction="column" spacing={1}>
+            <Typography variant="body2" sx={{color: 'text.secondary'}}>Type of content: {subscriptionContent}</Typography>
+          </Stack>
+        </CardContent>
+      </Card>
   );
 }
 
